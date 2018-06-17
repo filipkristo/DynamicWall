bf7da70f46f62859f65a8a2b914bbbc5e37d0d18
Asem Khen <moutasem19891@hotmail.com>
2018-06-18 01:57:11 +0200

    Add project files.

---
+++ BackgroundComponent/Class1.cs
@@ -0,0 +1,50 @@
+﻿using Windows.ApplicationModel.Background;
+using Windows.UI.Notifications;
+using Windows.System.UserProfile;
+using Windows.Storage;
+using System;
+using System.Threading.Tasks;
+using Windows.Storage.Pickers;
+using Windows.Storage.Streams;
+
+
+namespace BackgroundComponent
+{
+    public class Class1 : IBackgroundTask
+    {
+        
+        public void Run(IBackgroundTaskInstance taskInstance)
+        {
+            SendToast("Background task active");
+        }
+
+        public static void SendToast(string message)
+        {
+            var template = ToastTemplateType.ToastText01;
+            var xml = ToastNotificationManager.GetTemplateContent(template);
+            var elements = xml.GetElementsByTagName("Test");
+            var text = xml.CreateTextNode(message);
+
+            elements[0].AppendChild(text);
+            var toast = new ToastNotification(xml);
+            ToastNotificationManager.CreateToastNotifier().Show(toast);
+        }
+
+
+        // Pass in a relative path to a file inside the local appdata folder 
+        async Task<bool> SetWallpaperAsync()
+        {
+            bool success = false;
+            if (UserProfilePersonalizationSettings.IsSupported())
+
+            {
+                var imageId = DateTime.Now.Hour;
+                var uri = new Uri($"ms-appx:///Dynamic/Dynamic-{imageId}.jpeg");
+                StorageFile file = await StorageFile.GetFileFromApplicationUriAsync(uri);
+                success = await UserProfilePersonalizationSettings.Current.TrySetWallpaperImageAsync(file);
+                success = await UserProfilePersonalizationSettings.Current.TrySetLockScreenImageAsync(file);
+            }
+            return success;
+        }
+    }
+}
---
+++ BackgroundTaskDemo/MainPage.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.Background;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using System.Threading;
+using BackgroundComponent;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace BackgroundTaskDemo
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    /// <summary> 
+    /// An empty page that can be used on its own or navigated to within a Frame. 
+    /// </summary>
+
+    public sealed partial class MainPage : Page
+    {
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+         
+
+        public static async Task<BackgroundTaskRegistration> RegisterBackgroundTask(IBackgroundTrigger trigger)
+        {
+            var task = new BackgroundTaskBuilder
+            {
+                Name = "DynamicWall Process",
+                TaskEntryPoint = typeof(BackgroundComponent.Class1).ToString()
+            };
+            task.SetTrigger(trigger);
+
+            var Trigger = new ApplicationTrigger();
+            TimeTrigger hourlyTrigger = new TimeTrigger(60, false);
+            task.SetTrigger(hourlyTrigger);
+
+
+            var requestStatus = await Windows.ApplicationModel.Background.BackgroundExecutionManager.RequestAccessAsync();
+            if (requestStatus != BackgroundAccessStatus.AlwaysAllowed)
+            {
+                // Depending on the value of requestStatus, provide an appropriate response
+                // such as notifying the user which functionality won't work as expected
+            }
+
+            BackgroundTaskRegistration backtask = task.Register();
+            return backtask;
+        }
+        public static async void MainAsync()
+        {
+            var t = await Class1.SetWallpaperAsync()
+                { }
+                
+        }
+
+
+
+    }
+}
